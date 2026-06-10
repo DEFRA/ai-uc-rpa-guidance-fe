0e60dbfc5b8fc8401e89a3c8bc0798f80aab7445
Shaun Fitzsimons <87677173+sfitz42@users.noreply.github.com>
2026-06-10 15:37:04 +0100

    CAIT-151 - Align to AICE JS Standards (#1)
    * Apply template changes
    
    * Use ECS logging fields
    
    * Add missing import
    
    * Remove redundant script
    
    * Fix tests
    
    * Fix repo name typo

---
+++ src/server/catch-all.js
@@ -0,0 +1,44 @@
+import { statusCodes } from '../constants/status-codes.js'
+import { buildErrorLog } from '../infra/logging/utils/build-error-log.js'
+
+function statusCodeMessage (statusCode) {
+  switch (statusCode) {
+    case statusCodes.HTTP_STATUS_NOT_FOUND:
+      return 'Page not found'
+    case statusCodes.HTTP_STATUS_FORBIDDEN:
+      return 'Forbidden'
+    case statusCodes.HTTP_STATUS_UNAUTHORIZED:
+      return 'Unauthorized'
+    case statusCodes.HTTP_STATUS_BAD_REQUEST:
+      return 'Bad Request'
+    default:
+      return 'Something went wrong'
+  }
+}
+
+function catchAll (request, h) {
+  const { response } = request
+
+  if (!('isBoom' in response)) {
+    return h.continue
+  }
+
+  const statusCode = response.output.statusCode
+  const errorMessage = statusCodeMessage(statusCode)
+
+  if (statusCode >= statusCodes.HTTP_STATUS_INTERNAL_SERVER_ERROR) {
+    request.logger.error(buildErrorLog(response, { type: 'internal_server_error' }), 'Internal server error')
+  }
+
+  return h
+    .view('common/error', {
+      pageTitle: errorMessage,
+      heading: statusCode,
+      message: errorMessage
+    })
+    .code(statusCode)
+}
+
+export {
+  catchAll
+}
